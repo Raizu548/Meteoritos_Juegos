85084b036be8dc31ab356413b675eeb18bc84412
Raizu548 <rizzoluis53@gmail.com>
2022-07-07 17:15:51 -0300

    Movimiento y rotacion del personaje
    - Script de personaje.
    - atributos y metodos
    - movimiento
    - rotacion

---
+++ Juego/Naves/Player/Player.gd
@@ -0,0 +1,34 @@
+class_name Player
+extends RigidBody2D
+
+#Atributos export
+export var potencia_motor: int = 20
+export var potencia_rotacion: int = 200
+
+#Atributos
+var empuje: Vector2 = Vector2.ZERO
+var dir_rotacion: int = 0
+
+
+# Metodos
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	apply_torque_impulse(dir_rotacion * potencia_rotacion)
+	apply_central_impulse(empuje.rotated(rotation))
+
+func _process(delta: float) -> void:
+	player_input()
+	
+func player_input() -> void:
+	# Empuje
+	empuje = Vector2.ZERO
+	if Input.is_action_pressed("mover_adelante"):
+		empuje = Vector2(potencia_motor, 0)
+	elif Input.is_action_pressed("mover_atras"):
+		empuje = Vector2(-potencia_motor, 0)
+	
+	# Rotacion
+	dir_rotacion = 0
+	if Input.is_action_pressed("rotar_horario"):
+		dir_rotacion += 1
+	elif Input.is_action_pressed("rotar_antihorario"):
+		dir_rotacion -= 1
