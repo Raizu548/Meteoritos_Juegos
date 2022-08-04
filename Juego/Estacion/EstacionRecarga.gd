5ed75f54e8b7bb15707c4a85c8a7510325b95628
Raizu548 <rizzoluis53@gmail.com>
2022-08-04 19:33:25 -0300

    Creaccion de Base Enemiga
    - Base enemiga, Atraccion y colision.
    - Explosion

--- Juego/Estacion/EstacionRecarga.gd
+++ Juego/Estacion/EstacionRecarga.gd
@@ -49,12 +49,7 @@ func _on_AreaRecarga_body_entered(body):
 	if body is Player:
 		player_en_zona = true
 		nave_player = body
-		
-	body.set_gravity_scale(0.1)
-
 
 func _on_AreaRecarga_body_exited(body):
 	if body is Player:
 		player_en_zona = false
-	
-	body.set_gravity_scale(0.0)
