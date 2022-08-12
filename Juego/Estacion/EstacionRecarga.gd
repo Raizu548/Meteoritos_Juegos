9b68ea052b007416d51e3dad1defdfa610ac8c9e
Raizu548 <rizzoluis53@gmail.com>
2022-08-12 17:10:03 -0300

    Barras de salud y energia
    - Barra de salud de Player
    - Barra de salud de las naves enemigas
    - Barra de salud de la base enemiga
    - Barra de energia de la estacion de recarga.

--- Juego/Estacion/EstacionRecarga.gd
+++ Juego/Estacion/EstacionRecarga.gd
@@ -7,12 +7,17 @@ export var radio_energia_entregada: float = 0.05
 
 ## Atributos onready
 onready var carga_sfx: AudioStreamPlayer = $CargaSFX
+onready var barra_energia: ProgressBar = $BarraEnergia
 
 ## Atributos
 var nave_player: Player = null
 var player_en_zona: bool = false
 
 ## Metodos
+func _ready() -> void:
+	barra_energia.max_value = energia
+	barra_energia.value = energia
+
 func _unhandled_input(event: InputEvent) -> void:
 	if not puede_recargar(event):
 		return
@@ -43,6 +48,8 @@ func controlar_energia() -> void:
 	energia -= radio_energia_entregada
 	if energia <= 0.0:
 		$VacioSFX.play()
+	
+	barra_energia.value = energia
 
 ## Señales internas
 func _on_AreaColision_body_entered(body):
